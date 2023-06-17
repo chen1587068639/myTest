18f482f4de516aaa53682ec1e1750167ec4a74c1
chengaowei <chengaowei@zhongzhongtech.com>
2023-06-17 13:55:52 +0800

    解决依赖

--- src/test/java/com/example/test/algorithm/SortAlgorithm.java
+++ src/test/java/com/example/test/algorithm/SortAlgorithm.java
@@ -5,9 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Random;
 
 /**
  * 排序算法
