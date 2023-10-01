fe4c40e231e786eb18cde9519563edda15ea359c
chengaowei <chengaowei@zhongzhongtech.com>
2023-10-01 15:26:41 +0800

    对查找算法进行解释

--- src/test/java/com/example/test/algorithm/SearchElementAlgorithm.java
+++ src/test/java/com/example/test/algorithm/SearchElementAlgorithm.java
@@ -24,28 +24,25 @@ public class SearchElementAlgorithm {
     }
 
     /**
-     * 二分法查找
+     * 二分法查找：本质上是递归，将数组分为左右两个子数组，通过判断target与中间值做比较，判断在哪个数组，然后重复以上操作，直到分无可分，则确定数组中没有target
+     * 时间复杂度：log n
      * @param arrayList 数组
      * @param target 目标
      */
-    private static void dichotomySearch (List<Integer> arrayList,int target) {
-        int n = 0;
+    private static int dichotomySearch(List<Integer> arrayList,int target) {
         int startIndex = 0;
         int endIndex = arrayList.size() - 1;
         while (startIndex <= endIndex) {
-            n++;
             int middleIndex = (startIndex + endIndex) / 2;
             if (arrayList.get(middleIndex) == target) {
-                System.out.println("找到了，索引是：" + middleIndex + "，target：" + target);
-                System.out.println("二分法查找循环次数为:" + n + "次");
-                return;
+                return middleIndex;
             } else if (arrayList.get(middleIndex) > target) {
                 endIndex = middleIndex - 1;
             } else {
                 startIndex = middleIndex + 1;
             }
         }
-        System.out.println("结束循环，没有找到target");
+        return -1;
     }
 
     /**
@@ -71,21 +68,19 @@ public class SearchElementAlgorithm {
     }
 
     /**
-     * 顺序查找
+     * 顺序查找：对比数组中的每一个元素与target
      * @param arrayList 数组
      * @param target 目标
      */
-    private static void orderSearch (List<Integer> arrayList,int target) {
+    private static int orderSearch(List<Integer> arrayList,int target) {
         int n = 0;
         for (int i = 0; i < arrayList.size(); i++) {
             n++;
             if (target == arrayList.get(i)) {
-                System.out.println("目标数据的索引位置为:" + i + ",数字为:" + arrayList.get(i));
-                System.out.println("顺序查找循环次数为:" + n + "次");
-                return;
+                return i;
             }
         }
-        System.out.println("目标数据未在数组中");
+        return -1;
     }
 
 }
